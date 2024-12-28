edb983d3bf9541f4bf6700366526037ace8aba7e
Nycolase <nycolaswa@gmail.com>
2024-12-27 23:50:58 -0300

    Initial Commit

---
+++ alcool ou gasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alcool ou gasolina
+//
+//  Created by Nycolas William de Andrade on 27/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
